ebf545745231acd71598f5b5d2e702979f2327b9
kost960 <pozitq3@gmail.com>
2024-04-12 20:03:42 +0700

    Fix 8

--- Task 8.py
+++ Task 8.py
@@ -2,14 +2,14 @@ x = '123456789'
 sum = 0
 for a in range(1, 10):
     sum = int(x[:a]) * 8 + (a)
-    print(sum)
+    print(f'{x[:a]} * 8 + {a} = {sum}')
 print()
 sum2 = 0
 for b in range(1, 10):
     sum2 = int(x[:b]) * 9 + (b+1)
-    print(sum2)
+    print(f'{x[:b]} * 9 + {b+1} = {sum2}')
 print()
 z = '111111111'
 for c in range(1, 10):
     sum3 = int(z[:c])**2
-    print(sum3)
+    print(f'{z[:c]} * {z[:c]} = {sum3}')
